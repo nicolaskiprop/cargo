faf36c3936b3c1f352def07083ae9b392ebf055e
nicolaskiprop <yegonicholas@gmail.com>
2023-01-30 13:17:08 +0300

    immutable variables and variable shadowing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-     println!("the first letter of the english alphabet is {} and the last letter is {}.", 'A', 'Z');
-   
+    println!("the first letter of the english alphabet is {} and the last letter is {}.", 'A', 'Z');
+    let a_number;
+    let a_word = "Ten";
+    a_number = 10;
+    println!("This number is {}", a_number);
+    println!("the word is {}", a_word);
+
+    //shadowing
+    let shadow_num = 5;
+    let shadow_num = shadow_num + 5;
+    let shadow_num = shadow_num * 2;
+    println!("the number is {}", shadow_num)
+
 }
+
